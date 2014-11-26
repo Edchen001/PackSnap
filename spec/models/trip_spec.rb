b89f3f9c18cdc1f6b537ad2d586bad52a54e9039
Viktor Nikitenko <vnikiten01@gmail.com>
2014-11-26 11:52:53 -0500

    intial specs for Trip

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,1 +1,11 @@
 require 'rails_helper'
+
+describe Trip do
+  describe "validations" do
+    it { should validate_presenece_of :title }
+  end
+
+  describe "associations" do
+    it { should have_many :destinations }
+  end
+end
