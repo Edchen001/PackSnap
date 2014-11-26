b2f2b16c725a68a6b29098b1d55f99aa18d13f84
xiranou <xiranou@gmail.com>
2014-11-26 00:07:18 -0500

    initial destinations_controller

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,25 +1,51 @@
 class DestinationsController < ApplicationController
   def index
-
+    @destinations = Destination.all
   end
 
   def show
-
+    @destination = Destination.find(params[:id])
   end
 
-  def edit
-
+  def new
+    @destination = Destination.new
   end
 
   def create
+    @destination = Destination.new
+    @destination.assign_attributes(destination_params)
+    if @destination.save
+      redirect_to destination_path(@destination)
+    else
+      flash[:error] = @destination.errors.full_message
+      redirect_to new_destination_path(@destination)
+    end
+  end
 
+  def edit
+    @destination = Destination.find(params[:id])
   end
 
   def update
-
+    @destination = Destination.find(params[:id])
+    @destination.assign_attributes(destination_params)
+    if @destination.save
+      redirect_to destination_path(@destination)
+    else
+      flash[:error] = @destination.errors.full_message
+      redirect_to edit_destination_path(@destination)
+    end
   end
 
   def destroy
+    @destination = Destination.find(params[:id])
+    @destination.delete
+    redirect_to destinations_path
+  end
 
+  private
+
+  def destination_params
+    params.require(:destination).permit(:location,:start_date, :end_date)
   end
 end
