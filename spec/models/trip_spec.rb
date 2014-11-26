55f38722efef3b99a994542970b21bdfd86b75c4
Viktor Nikitenko <vnikiten01@gmail.com>
2014-11-26 11:54:17 -0500

    update trip_spec

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Trip do
   describe "validations" do
     it { should validate_presenece_of :title }
+    it { should validate_presenece_of :description }
   end
 
   describe "associations" do
