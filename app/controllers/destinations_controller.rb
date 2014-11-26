5f8b93647de7fce8e1b4a18929cbfbc7f2cd7a35
xiranou <xiranou@gmail.com>
2014-11-25 21:43:59 -0500

    add skeleton destinations_controller

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,3 +1,25 @@
 class DestinationsController < ApplicationController
+  def index
 
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
