1cf25062fe5d51c61b32ec749d1d04084aae8b48
xiranou <xiranou@gmail.com>
2014-11-26 00:06:09 -0500

    update factory with invalid_destination

--- spec/factories/destination.rb
+++ spec/factories/destination.rb
@@ -5,5 +5,9 @@ FactoryGirl.define do
     location "Canton"
     start_date Date.new(2014,11,25)
     end_date Date.new(2014,11,30)
+
+    factory :invalid_destination do
+      location nil
+    end
   end
 end
