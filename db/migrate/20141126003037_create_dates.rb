af23440d38d9f546e956fdb41bf60793bf2f75c7
Viktor Nikitenko <vnikiten01@gmail.com>
2014-11-25 19:50:23 -0500

    Database tables written, next work on models

--- db/migrate/20141126003037_create_dates.rb
+++ db/migrate/20141126003037_create_dates.rb
@@ -1,6 +1,11 @@
 class CreateDates < ActiveRecord::Migration
   def change
     create_table :dates do |t|
+      t.date :current_date , null: false
+      t.references :destination, null: false
+      t.references :weather, null: false
+
+      t.timestamps
     end
   end
 end
--- db/migrate/20141126003041_create_weathers.rb
+++ db/migrate/20141126003041_create_weathers.rb
@@ -1,6 +1,8 @@
 class CreateWeathers < ActiveRecord::Migration
   def change
     create_table :weathers do |t|
+      t.decimal :fahrenheit
+      t.decimal :celsius
     end
   end
 end
