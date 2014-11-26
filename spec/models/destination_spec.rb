739b127fd79599d6b2d3c576c0b1241681e2ea8a
Viktor Nikitenko <vnikiten01@gmail.com>
2014-11-26 11:44:06 -0500

    Rspec model destination

--- spec/models/destination_spec.rb
+++ spec/models/destination_spec.rb
@@ -1,1 +1,15 @@
 require "rails_helper"
+
+describe Destination do
+  describe "Associations for desitination model" do
+   it {should have_many(:travel_dates)}
+   it {should have_many(:weathers)}
+  end
+
+  describe "Have validations" do
+    it {should validate_presence_of :location}
+    it {should validate_presence_of :start_date}
+    it {should validate_presence_of :end_date}
+  end
+
+end
