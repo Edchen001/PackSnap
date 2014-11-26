3b96448d5bc146f0d50123e6c15d789f9897b84b
Viktor Nikitenko <vnikiten01@gmail.com>
2014-11-26 12:02:16 -0500

    add trip_id migration for destinations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141126165311) do
+ActiveRecord::Schema.define(version: 20141126170140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20141126165311) do
     t.date     "end_date",   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "trip_id"
   end
 
   create_table "travel_dates", force: true do |t|
