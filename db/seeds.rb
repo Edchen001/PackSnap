5bd10f4af95f93e11d411699a6322353751859f1
xiranou <xiranou@gmail.com>
2014-11-27 16:13:43 -0500

    add require csv in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+require 'date'
+
 first = Trip.create(title: "TRIP 1")
 
 iti_first = first.itineraries.create(start_date:"1000-01-02", end_date:"1111-11-03")
