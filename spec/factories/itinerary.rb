df57d982e7114b135c7ec24c7099cf41f54efa40
xiranou <xiranou@gmail.com>
2014-11-27 16:23:19 -0500

    update itinerary factory with association => trip

--- spec/factories/itinerary.rb
+++ spec/factories/itinerary.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
   factory :itinerary do
     start_date Date.new(2014,11,25)
     end_date Date.new(2014,11,30)
+    association :trip, factory: :trip
 
     factory :invalid_itinerary do
       start_date nil
