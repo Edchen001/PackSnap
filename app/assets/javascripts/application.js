0568420ccc6959705a8c2df9bb0c75258079a1ab
aj9045 <identity_unveiled@hotmail.com>
2014-11-26 19:55:52 -0500

    render banner partial

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require turbolinks
 //= require_tree .
 
+
 $(function(){
 	$("#geocomplete").geocomplete({
 		map: ".map_canvas",
@@ -26,3 +27,16 @@ $(function(){
 		$("#geocomplete").trigger("geocode");
 	});
 });
+
+$(document).ready(function() {
+
+   $('.banner').unslider({
+    speed: 400,
+    complete: function() {},
+    keys: true,
+    fluid: true
+  });
+   var unslider = $('.banner').unslider(),
+         data = unslider.data('unslider');
+    window.onload = function () {  data.move(0); }
+})
