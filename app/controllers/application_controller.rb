9918b20d134279fb325c7e80d8f78ef48f7fc4c7
xiranou <xiranou@gmail.com>
2014-11-27 18:19:49 -0500

    add flash[:alert] for invalid attribute submission

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def set_alert(object_with_error)
+    flash.now.alert = object_with_error.errors.full_messages
+  end
 end
