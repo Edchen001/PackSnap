6b31c7cbb2eb7c4e73898ca15b7b6b0d08df17f1
Baron Willeford <baron.willeford@gmail.com>
2014-11-28 14:30:34 -0500

    seeding coordinates now works

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,14 @@ require 'date'
 # # Weather.create(fahrenheit: 45)
 # # Destination.first.travel_dates.create(current_date:"3453-02-02", weather_id: Weather.first.id)
 
-# ClimateType.create(code: 'Af', name: 'Tropical rainforest climate', description: 'hot and wet')
-
-# File.open('../app/assets/kg.txt') { |f| f.each_line { |l| l.split(/\s+/) } }
-
 
 CSV.foreach('app/assets/data_files/climates2.csv', headers: true, header_converters: :symbol, converters: :all) do |csv|
 	ClimateType.create(csv.to_hash)
 end
+
+File.readlines('app/assets/data_files/kg.txt').drop(1).each do |f|
+    coord = f.split(/\s+/)
+    ClimateType.find_by(code: coord[3]).coordinates.create(latitude: coord[1], longitude: coord[2])
+end
+
+
