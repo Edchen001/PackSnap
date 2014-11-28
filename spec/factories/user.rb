9db1d834193e328c49fe2d3939431b6f7026c8e5
xiranou <xiranou@gmail.com>
2014-11-28 11:53:50 -0500

    update user specs

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    username "test"
+    username "testdfghjgh"
     email "test@dbc.com"
     password "test"
+    password_confirmation "test"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 describe User do
   describe "validation" do
+    subject { build(:user) }
+    it { should have_secure_password }
     it { should validate_presence_of :email }
-    it { should validate_presence_of :password }
+    it { should validate_uniqueness_of :email }
     it { should validate_presence_of :username }
   end
 
@@ -12,7 +14,7 @@ describe User do
     it { should have_many(:itineraries) }
     it { should have_many(:itineraries).through(:trips) }
     it { should have_many(:destinations) }
-    it { should have_many(:destinations).through(:trips) }
-    it { should have_many(:destinations).through(:trips).as(:locations) }
+    it { should have_many(:destinations).through(:itineraries) }
+    it { should have_many(:destinations).through(:itineraries).source(:locations) }
   end
 end
