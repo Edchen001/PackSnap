7fa0f1aac288ad5f39652d62093dc437c6bc0173
xiranou <xiranou@gmail.com>
2014-11-28 16:59:44 -0500

    update users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,43 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render :signup
+  end
+  def show
+    @user = User.find(session[:user_id])
+  end
+  def edit
+    @user = User.find(session[:user_id])
+  end
+  def update
+    @user = User.find(session[:user_id])
+    @user.assign_attributes(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      set_alert(@user)
+      render :edit
+    end
+  end
+  def create
+    @user = User.new
+    @user.assign_attributes(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      set_alert(@user)
+      render :signup
+    end
+  end
+  def destroy
+    @user = User.find(session[:user_id])
+    @user.destroy
+    redirect_to root_path
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirm)
+  end
 end
