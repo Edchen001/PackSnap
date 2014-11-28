1f0cda68aa9988d132e1117a71131b29588494bf
xiranou <xiranou@gmail.com>
2014-11-28 16:59:26 -0500

    update users_controller_spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,11 @@
 require "rails_helper"
 
 describe UsersController do
+  before do
+    @user = create(:user)
+    session[:user_id] = @user.id
+  end
   describe "Get routes" do
-    before do
-      @user = create(:user)
-    end
     describe "Get #show" do
       it "should locate the requested user" do
         get :show, id: @user
@@ -55,7 +56,7 @@ describe UsersController do
       end
       it "should set up flash alert" do
         post :create, user: attributes_for(:invalid_user)
-        expect(response.requested.flash[:alert]).to_not be_nil
+        expect(response.request.flash[:alert]).to_not be_nil
       end
       it "should re-render :signup template" do
         post :create, user: attributes_for(:invalid_user)
@@ -65,9 +66,6 @@ describe UsersController do
   end
 
   describe "Put #update" do
-    before do
-      @user = create(:user)
-    end
     context 'valid attributes' do
       it "should locate requested user" do
         put :update, id: @user, user: attributes_for(:user)
@@ -101,16 +99,13 @@ describe UsersController do
   end
 
   describe "Delete #destroy" do
-    before :each do
-      @user = create(:user)
-    end
     it "should locate the requested user" do
       delete :destroy, id: @user
       expect(assigns[:user]).to eq(@user)
     end
     it "should validate its the same as the logged-in user" do
       delete :destroy, id: @user
-      expect(session[:user_id]).to eq(@user)
+      expect(session[:user_id]).to eq(@user.id)
     end
     it "should remove user from database" do
       expect { delete :destroy, id: @user }.to change(User, :count).by(-1)
