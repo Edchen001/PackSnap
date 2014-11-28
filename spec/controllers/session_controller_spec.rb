bc84d5d6db7f8f0fec1dc03017ff8f7d200b93ab
xiranou <xiranou@gmail.com>
2014-11-28 14:20:01 -0500

    add session_controller_spec

---
+++ spec/controllers/session_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe SessionController do
+  describe "Get#new" do
+    it "should render login" do
+      get :new
+      expect(response).to render_template(:login)
+    end
+  end
+
+  describe "Post#login" do
+    before do
+      @user = create(:user)
+    end
+    it "should set session with user id" do
+      post :login
+      expect(session[:user_id]).to eq(@user.id)
+    end
+    it "should redirect to root_path" do
+      post :login
+      expect(response).to redirect_to(root_path)
+    end
+  end
+
+  describe "Delete#logout" do
+    it "should set session[:user_id] back to nil" do
+      delete :logout
+      expect(session[:user_id]).to be_nil
+    end
+  end
+end
