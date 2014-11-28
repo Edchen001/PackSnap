3ef2478069707a336892d2979ef76bf53cfc6ad2
xiranou <xiranou@gmail.com>
2014-11-27 20:43:14 -0500

    separate out js files

---
+++ app/assets/javascripts/geo_query.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+  $("#geocomplete").geocomplete({
+    map: ".map_canvas",
+    details: "#map_info",
+    detailsAttribute: "data-geo"
+  });
+
+  $('body').on('click', '#find', function(e) {
+    e.preventDefault();
+    $("#geocomplete").trigger("geocode")
+    .bind('geocode:result', function(e, result) {
+      console.log(e.target);
+      console.log(result.geometry.location.lat());
+      console.log(result.geometry.location.lng());
+    });
+  });
+
+
+  $('.banner').unslider({
+    speed: 400,
+    complete: function() {},
+    keys: true,
+    fluid: true
+  });
+  var unslider = $('.banner').unslider(),
+  data = unslider.data('unslider');
+  window.onload = function () {  data.move(0); };
+});
