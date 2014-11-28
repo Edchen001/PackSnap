2421e7745bbb593a4f2c3ee5fbd2c26ca9177b1d
xiranou <xiranou@gmail.com>
2014-11-28 14:30:51 -0500

    update session_controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,13 +1,16 @@
 class SessionController < ApplicationController
   def new
-
+    render :login
   end
 
   def login
-
+    @user = User.find_by(email: params[:user][:email]).try(:authenticate, params[:user][:password])
+    session[:user_id] = @user.id
+    redirect_to root_path
   end
 
   def logout
-
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
