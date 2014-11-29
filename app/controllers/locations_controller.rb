fd9313f893b24ffa5a8264ff57d0f27f7390f47a
xiranou <xiranou@gmail.com>
2014-11-29 00:48:10 -0500

    made location_params private

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,6 +3,7 @@ class LocationsController < ApplicationController
     @location = Location.new()
     render :new, locals:{location: @location}
   end
+
   def create
     itinerary_id = flash[:itinerary_id]
     attributes = location_params
@@ -10,12 +11,15 @@ class LocationsController < ApplicationController
     @location = Location.new(itinerary_id: itinerary_id, address: attributes[:address], coordinate_id: @coordinate.id)
     if @location.save
       flash[:itinerary_id] = nil
+    if @location.save!
       redirect_to trip_itinerary_path(@location.itinerary.trip, @location.itinerary)
     else
       set_alert(@location)
       render :new, locals:{location: @location}
     end
   end
+
+  private
 
   def location_params
     params.require(:geoInfo).permit(:address, :latitude, :longitude)
