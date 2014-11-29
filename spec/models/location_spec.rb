034547c433414f7411a94246f5a3c780efa3a336
xiranou <xiranou@gmail.com>
2014-11-29 00:28:52 -0500

    update location specs

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Location do
   describe "validations" do
-    it { should validate_presence_of :name }
+    it { should validate_presence_of :address }
   end
   describe "associations" do
     it { should belong_to :itinerary }
