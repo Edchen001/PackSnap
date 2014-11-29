0a643c478bf9ad4d6ed1454464217dcf50dbe9e5
Viktor Nikitenko <vnikiten01@gmail.com>
2014-11-28 20:48:49 -0500

    Create updatelocation tale migration

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,10 +4,10 @@ class LocationsController < ApplicationController
     render :new, locals:{location: @location}
   end
   def create
-    p params
-    raise (params);
+    p location_params
+    # raise (params);
     @location = Location.new(itinerary_id: session[:itinerary_id])
-    @location.assign_attributes(params[:location])
+    @location.assign_attributes(location_params)
     if @location.save
       session[:itinerary_id] = nil
       redirect_to trip_itinerary_path(@location.itinerary.trip, @location.itinerary)
@@ -15,5 +15,9 @@ class LocationsController < ApplicationController
       set_alert(@location)
       render :new, locals:{location: @location}
     end
+  end
+
+  def location_params
+    params.require(:geoInfo).permit(:address)
   end
 end
