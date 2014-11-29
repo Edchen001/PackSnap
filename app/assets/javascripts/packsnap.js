c9f096fe57d05704fc4f0229f33633639422dda5
xiranou <xiranou@gmail.com>
2014-11-29 12:04:06 -0500

    merge with master

--- app/assets/javascripts/packsnap.js
+++ app/assets/javascripts/packsnap.js
@@ -1,4 +1,5 @@
 $(function(){
+
   $("#geocomplete").geocomplete({
     map: ".map_canvas",
     details: "#map_info",
@@ -6,68 +7,30 @@ $(function(){
   });
 
 
-
-
   $('.new-location').submit(function(e){
+
     var $form = $(e.target);
     e.preventDefault();
+
     $("#geocomplete").trigger("geocode").bind('geocode:result', function(e, result){
-      var geoInfo = {};
-      console.log(result.address_components);
-      geoInfo.latitude = result.geometry.location.lat();
-      geoInfo.longitude = result.geometry.location.lng();
-      geoInfo.address = $("#geocomplete").val();
-      $.ajax({
-         url: $form.attr('action'),
-         type: $form.attr('method'),
-         dateType: 'html',
-         data: {geoInfo: geoInfo}
-       })
-        .done(function(response){
-          console.log(response);
-          $('body').append(response);
-        });
+
+        var geoInfo = {};
+        console.log(result.address_components);
+        geoInfo.latitude = result.geometry.location.lat();
+        geoInfo.longitude = result.geometry.location.lng();
+        geoInfo.address = $("#geocomplete").val();
+
+        $.ajax({
+           url: $form.attr('action'),
+           type: $form.attr('method'),
+           dateType: 'html',
+           data: {geoInfo: geoInfo}
+        })
+      .done(function(response){
+        console.log(response);
+        $('body').append(response);
+      });
     });
-  }
-
-
-
-
-    $('.banner').unslider({
-      speed: 400,
-      complete: function() {},
-      keys: true,
-      fluid: true
-    });
-
-    var unslider = $('.banner').unslider(),
-    data = unslider.data('unslider');
-    window.onload = function () {  data.move(0); };
   });
 
-
-    // {
-    //   e.preventDefault();
-    //   geoLocation = result
-    //   console.log($(e.target));
-    //   var geoInfo ={};
-    //   geoInfo.lat = result.geometry.location.lat();
-    //   geoInfo.lng = result.geometry.location.lng();
-    //   geoInfo.city = result.address_components[2];
-    //   geoInfo.country = result.address_components[5];
-    //   var startDate = $form.find(".itinerary_start_date_1i").val();
-    //   console.log(startDate);
-    //   // console.log ($form.serialize());
-
-    //   // console.log($.param(result));
-
-
-
-    //   // // console.log(e.target);
-    //   console.log(result.geometry.location.lat());
-    //   console.log(result.geometry.location.lng());
-    //   // console.log(result.address_components[0]);
-    //   // console.log(result.address_components[1]);
-    //   // console.log(result.address_components[2]);
-    //   // console.log(result.address_components[3]);
-    //   // console.log(result.address_components[4]);
+});
