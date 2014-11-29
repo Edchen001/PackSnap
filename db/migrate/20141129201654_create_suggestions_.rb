822b45a4bb14e721e6aba69b20229abf06762d7f
Viktor Nikitenko <vnikiten01@gmail.com>
2014-11-29 15:19:26 -0500

    migration suggestions written

--- db/migrate/20141129201654_create_suggestions_.rb
+++ db/migrate/20141129201654_create_suggestions_.rb
@@ -1,6 +1,8 @@
 class CreateSuggestions < ActiveRecord::Migration
   def change
     create_table :suggestions_s do |t|
+    	t.references :item
+    	t.references :temperature_category
     end
   end
 end
