ba62b1bc60ad770a2d3fdd7d6f3ab3effcb04f62
xiranou <xiranou@gmail.com>
2014-11-28 23:00:40 -0500

    fix bug passing itinerary_id to locations_controller, not redirecting correctly

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,13 +4,11 @@ class LocationsController < ApplicationController
     render :new, locals:{location: @location}
   end
   def create
-    puts "*" * 50
-    p session[:itinerary_id]
+    itinerary_id = flash[:itinerary_id]
     attributes = location_params
     @coordinate = Coordinate.find_or_create_by(latitude:attributes[:latitude], longitude: attributes[:longitude])
-    @location = Location.new(itinerary_id: session[:itinerary_id], address: attributes[:address], coordinate_id: @coordinate.id)
+    @location = Location.new(itinerary_id: itinerary_id, address: attributes[:address], coordinate_id: @coordinate.id)
     if @location.save
-      session[:itinerary_id] = nil
       redirect_to trip_itinerary_path(@location.itinerary.trip, @location.itinerary)
     else
       set_alert(@location)
