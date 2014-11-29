ee3c5ea6aab5065d83fa1292e12898cedf2e7f9e
xiranou <xiranou@gmail.com>
2014-11-29 11:31:30 -0500

    able to create location and coordinates, render back partial with ajax

--- app/assets/javascripts/packsnap.js
+++ app/assets/javascripts/packsnap.js
@@ -12,32 +12,38 @@ $(function(){
     var $form = $(e.target);
     e.preventDefault();
     $("#geocomplete").trigger("geocode").bind('geocode:result', function(e, result){
-        var geoInfo = {};
-        console.log(result.address_components);
-        geoInfo.latitude = result.geometry.location.lat();
-        geoInfo.longitude = result.geometry.location.lng();
-        geoInfo.address = $("#geocomplete").val();
-       $.ajax({
-           url: $form.attr('action'),
-           type: $form.attr('method'),
-           data: {geoInfo: geoInfo}
-         })
-       .done(function(response){
-         window.location.href = response.redirect;
-       });
+      var geoInfo = {};
+      console.log(result.address_components);
+      geoInfo.latitude = result.geometry.location.lat();
+      geoInfo.longitude = result.geometry.location.lng();
+      geoInfo.address = $("#geocomplete").val();
+      $.ajax({
+         url: $form.attr('action'),
+         type: $form.attr('method'),
+         dateType: 'html',
+         data: {geoInfo: geoInfo}
+       })
+        .done(function(response){
+          console.log(response);
+          $('body').append(response);
+        });
     });
   }
-  $('.banner').unslider({
-    speed: 400,
-    complete: function() {},
-    keys: true,
-    fluid: true
-  });
 
-  var unslider = $('.banner').unslider(),
-  data = unslider.data('unslider');
-  window.onload = function () {  data.move(0); };
-});
+
+
+
+    $('.banner').unslider({
+      speed: 400,
+      complete: function() {},
+      keys: true,
+      fluid: true
+    });
+
+    var unslider = $('.banner').unslider(),
+    data = unslider.data('unslider');
+    window.onload = function () {  data.move(0); };
+  });
 
 
     // {
