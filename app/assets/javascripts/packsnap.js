d251c05555e8e314abe5f12e841b24a59bcb4ba1
xiranou <xiranou@gmail.com>
2014-11-29 13:49:30 -0500

    merge with master

--- app/assets/javascripts/packsnap.js
+++ app/assets/javascripts/packsnap.js
@@ -1,22 +1,22 @@
 $(function(){
+
   $("#geocomplete").geocomplete({
     map: ".map_canvas",
     details: "#map_info",
     detailsAttribute: "data-geo"
   });
 
-
-
-
   $('.new-location').submit(function(e){
     var $form = $(e.target);
     e.preventDefault();
+
     $("#geocomplete").trigger("geocode").bind('geocode:result', function(e, result){
+
       var geoInfo = {};
-      console.log(result.address_components);
       geoInfo.latitude = result.geometry.location.lat();
       geoInfo.longitude = result.geometry.location.lng();
       geoInfo.address = $("#geocomplete").val();
+
       $.ajax({
          url: $form.attr('action'),
          type: $form.attr('method'),
@@ -24,24 +24,23 @@ $(function(){
          data: {geoInfo: geoInfo}
        })
         .done(function(response){
-          console.log(response);
           $('body').append(response);
         });
     });
-  }
+  });
 
 
 
 
-    $('.banner').unslider({
-      speed: 400,
-      complete: function() {},
-      keys: true,
-      fluid: true
-    });
+  $('.banner').unslider({
+    speed: 400,
+    complete: function() {},
+    keys: true,
+    fluid: true
+  });
 
-    var unslider = $('.banner').unslider(),
-    data = unslider.data('unslider');
-    window.onload = function () {  data.move(0); };
+  var unslider = $('.banner').unslider(),
+  data = unslider.data('unslider');
+  window.onload = function () { data.move(0); };
 
 });
