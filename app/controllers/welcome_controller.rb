a911ebc0bef2208056ee4cea17d5b48e415ec171
xiranou <xiranou@gmail.com>
2014-11-29 16:13:46 -0500

    add dash function, with private helper methods

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,20 @@ class WelcomeController < ApplicationController
   def index
 
   end
+
+  def dash
+    @users = get_users(coordinate)
+    render :dash, locals:{users: @users}
+  end
+
+  private
+
+  def inputted_destinations(coordinate)
+    coordinate = Coordinate.find_by(longitude: coordinate[:longitude], latitude: coordinate[:latitude])
+    coordinate.destinations
+  end
+
+  def get_users(coordinate)
+    inputted_destinations(coordinate).map { |destination| destination.user }
+  end
 end
