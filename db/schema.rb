7e4410613d134705afa902965de84d90eba90af9
xiranou <xiranou@gmail.com>
2014-11-29 15:06:55 -0500

    fix log in form

--- db/schema.rb
+++ db/schema.rb
@@ -61,14 +61,10 @@ ActiveRecord::Schema.define(version: 20141129022524) do
 
   create_table "users", force: true do |t|
     t.string   "username"
-    t.string   "email",            null: false
-    t.string   "password_digest",  null: false
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "bag_file_name"
-    t.string   "bag_content_type"
-    t.integer  "bag_file_size"
-    t.datetime "bag_updated_at"
   end
 
   create_table "weathers", force: true do |t|
