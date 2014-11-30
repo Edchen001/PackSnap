051d6539225bbb48d64bdc3786155d7f24cf1512
Viktor Nikitenko <vnikiten01@gmail.com>
2014-11-30 11:25:25 -0500

    Add paperclip avatar to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ActiveRecord::Base
 	# validates :name, :url, presence: true
 	has_many :suggestions
 	has_many :categories, through: :suggestions 
+
+
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/public/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
