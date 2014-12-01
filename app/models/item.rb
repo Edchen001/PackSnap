a59907a92caa5fd4a139e01d11764f28413002e7
xiranou <xiranou@gmail.com>
2014-11-30 22:04:18 -0500

    initial seed without pictures

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,6 @@ class Item < ActiveRecord::Base
 	has_many :categories, through: :suggestions
 
   has_attached_file :photo, styles: { :medium => "300x300!", :thumb => "100x100>" }
+  do_not_validate_attachment_file_type :photo
+  validates_attachment_content_type :photo, :content_type => "jpg"
 end
