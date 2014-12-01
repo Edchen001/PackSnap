88737dbcab9e3aab60321f4d2aafebb69ac10247
xiranou <xiranou@gmail.com>
2014-12-01 12:32:22 -0500

    update model assications for user, location, item

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,7 @@ class Location < ActiveRecord::Base
   validates_presence_of :address
   belongs_to :itinerary
   belongs_to :coordinate
+
+  has_many :photos
+  has_many :users, through: :photos
 end
