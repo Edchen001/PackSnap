439bd085f7d6bd0a4dd4854f2a4dcf2eb9daf2d7
xiranou <xiranou@gmail.com>
2014-12-01 12:28:50 -0500

    add association to photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,8 @@
 class Photo < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :location
+  has_many :items
 
+  has_attached_file :image, styles: { :medium => "300x300!", :thumb => "100x100>" }, whiny: false
+  do_not_validate_attachment_file_type :image
 end
