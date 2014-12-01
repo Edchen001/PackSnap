e2e00f57815851d2e93a4bfff1999129e12fba2b
xiranou <xiranou@gmail.com>
2014-12-01 14:08:40 -0500

    update seed file to include Photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.create(username: "xiran", email: "xiran@dbc.com", password: 123, password_confirmation: 123)
+xiran = User.create(username: "xiran", email: "xiran@dbc.com", password: 123, password_confirmation: 123)
 
 
 blazing = Category.create(name:"blazing")
@@ -15,10 +15,15 @@ cool.scope = Scope.create(minimum:50, maximum:69)
 cold.scope = Scope.create(minimum: 33, maximum:49)
 freezing.scope = Scope.create(minimum: -30, maximum:32)
 
-jean = Item.create!(name: "jean", url:"www.jean.com", photo: File.open('app/assets/images/jean_image.jpg'))
-snowjacket = Item.create!(name:"snowjacket", url:"snowjacket.com", photo: File.open('app/assets/images/snow_jacket.jpg'))
-sunglasses = Item.create!(name:"sunglasses", url:"sun_glasses.com", photo: File.open('app/assets/images/sun_glasses.jpg'))
-sunhat = Item.create!(name:"sunhat", url:"sunhat.com", photo: File.open('app/assets/images/sun_hat.jpg'))
+jean_image = Photo.create!(image: File.open('app/assets/images/jean_image.jpg'))
+snowjacket_image = Photo.create!(image: File.open('app/assets/images/snow_jacket.jpg'))
+sunglasses_image = Photo.create!(image: File.open('app/assets/images/sun_glasses.jpg'))
+sunhat_image = Photo.create!(image: File.open('app/assets/images/sun_hat.jpg'))
+
+jean = Item.create!(name: "jean", url:"www.jean.com", photo: jean_image)
+snowjacket = Item.create!(name:"snowjacket", url:"snowjacket.com", photo: snowjacket_image)
+sunglasses = Item.create!(name:"sunglasses", url:"sun_glasses.com", photo: sunglasses_image)
+sunhat = Item.create!(name:"sunhat", url:"sunhat.com", photo: sunhat_image)
 
 Suggestion.create(item: jean, category: cool)
 Suggestion.create(item: jean, category: warm)
