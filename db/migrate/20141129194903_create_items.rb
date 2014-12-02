6f0a68ed44aca7f0e25ad4071c2172c585b9f2a9
xiranou <xiranou@gmail.com>
2014-12-01 19:08:35 -0500

    update migrations constrains

--- db/migrate/20141129194903_create_items.rb
+++ db/migrate/20141129194903_create_items.rb
@@ -1,10 +1,11 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
-    	t.string :name
+    	t.string :name, null: false
+      t.text :description
       t.attachment :image
       t.belongs_to :user
-      t.belongs_to :suggestable, polymorphic: true
+      t.belongs_to :suggestable, polymorphic: true, null: false
 
       t.timestamps
     end
