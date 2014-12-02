9a8cadefda18c6998bb9800a52af69f105e420c8
baron816 <baron.willeford@gmail.com>
2014-12-02 10:36:34 -0500

    validates presence of password only on create and not edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates_presence_of :username, :email, :password
+  validates_presence_of :username, :email
+  validates_presence_of :password, on: :create
   validates_uniqueness_of :email
 
   has_many :items
