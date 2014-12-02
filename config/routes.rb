3d1b0eeb3b596143e2ac9a33e6434ca53739e435
edchen001 <edchen001@gmail.com>
2014-12-01 22:52:16 -0500

    add temp route for dash in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :users, except: :index
   resources :locations, only: [:create, :new]
   resources :photos, only: [:create, :new]
+  get "dashboard" => "welcome#dashboard"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
