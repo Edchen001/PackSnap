5321519ade3b4bb882d18dd9a16b2fef8c8df388
AJ Stuhrenberg <identity_unveiled@hotmail.com>
2014-12-03 09:40:20 -0500

    update location model spec (associations)

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -4,4 +4,10 @@ describe Location do
   describe "validations" do
     it { should validate_presence_of :address }
   end
+
+  describe "associations" do
+    it { should have_many :items }
+    it { should have_many :comments }
+    it { should have_many :visitors }
+  end
 end
