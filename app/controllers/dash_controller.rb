6689bac957c2fda9f02b7af4125f0cec774379df
AJ Stuhrenberg & Baron Willeford <pair+aj9045.baron816@devbootcamp.com>
2014-12-03 12:13:39 -0500

    refactored create method

--- app/controllers/dash_controller.rb
+++ app/controllers/dash_controller.rb
@@ -1,13 +1,13 @@
 class DashController < ApplicationController
 
   def index
-    puts params
     @user = User.new
 
     @location = Location.find_or_create_by(location_params)
     session[:location] = @location.id
     weather_info = Forecast.new(params[:forecast])
     @weather = weather_info.current_weather
+    puts @weather
     scope = Scope.get_weather_scope(@weather)
     @suggested_items = Category.unique_items(scope, weather_info.precipation_type)
     @summary = weather_info.summary
@@ -26,10 +26,9 @@ class DashController < ApplicationController
   end
 
   def create
-    @user = User.find(session[:user_id])
-    @location = Location.find(location_params[:id])
-    @item = Item.new(location: @location)
-    @comment = Comment.new(user: @user, location: @location, item: @item)
+    @item = Item.new(location_id: location_params[:id])
+    @comment = Comment.new(user: session[:user_id], location_id: location_params[:id], item: @item)
+
     @comment.assign_attributes(comment_params)
     @item.assign_attributes(item_params)
 
