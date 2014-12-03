5ab3c07e79ddd3c0930d53bfc13e94baeaca5e5d
AJ Stuhrenberg <identity_unveiled@hotmail.com>
2014-12-03 10:05:05 -0500

    update item model spec with paperclip attrs

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 describe Item do
     describe "validations" do
     it { should validate_presence_of :name }
+    it { should have_attached_file(:image) }
+    it { should validate_attachment_size(:thumb) }
+    it { should validate_attachment_size(:medium) }
+
   end
   describe "associations" do
     it { should belong_to :user }
