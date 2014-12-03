0d50b4149c04352616beed0716d2add5c0bbc342
xiranou <xiranou@gmail.com>
2014-12-02 23:27:19 -0500

    update tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,13 +8,4 @@ describe User do
     it { should validate_uniqueness_of :email }
     it { should validate_presence_of :username }
   end
-
-  describe "association" do
-    it { should have_many(:trips) }
-    it { should have_many(:itineraries) }
-    it { should have_many(:itineraries).through(:trips) }
-    it { should have_many(:destinations) }
-    it { should have_many(:destinations).through(:itineraries) }
-    it { should have_many(:destinations).through(:itineraries).source(:location) }
-  end
 end
