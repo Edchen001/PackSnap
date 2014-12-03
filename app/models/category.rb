1eae00830138cac98d8f26b57ef50b4afd08e416
AJ Stuhrenberg & Baron Willeford <pair+aj9045.baron816@devbootcamp.com>
2014-12-03 10:06:17 -0500

    refactored code from refactor2

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,17 @@ class Category < ActiveRecord::Base
 	has_many :suggestions
 	has_many :items, through: :suggestions
 	has_one :scope
+
+  def self.get_suggested_precipitation_type_item(precipitation_type)
+    categories = self.where(name: precipitation_type)
+    categories ? [] : categories.items
+  end
+
+  def self.get_suggested_weather_items(scope)
+    scope.category.items
+  end
+
+  def self.unique_items(scope, precipitation_type)
+    (self.get_suggested_weather_items(scope) + self.get_suggested_precipitation_type_item(precipitation_type)).uniq
+  end
 end
