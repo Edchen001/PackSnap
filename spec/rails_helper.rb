af51f8c3efcfb61108ff68834b4b8c1e8261460b
AJ Stuhrenberg <identity_unveiled@hotmail.com>
2014-12-03 10:02:41 -0500

    edit requirement of paperclip matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,9 +7,7 @@ require 'shoulda/matchers'
 require 'paperclip/matchers'
 
 # Add additional requires below this line. Rails is not loaded until this point!
-RSpec.configure do |config|
-  config.include Paperclip::Shoulda::Matchers
-end
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -33,6 +31,7 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  config.include Paperclip::Shoulda::Matchers
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
