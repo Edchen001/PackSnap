0b891debf8e8cc1baa724650427ee011ecaebffa
AJ Stuhrenberg <identity_unveiled@hotmail.com>
2014-12-03 09:49:20 -0500

    add scope model spec

---
+++ spec/models/scope_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe Suggestion do
+  describe "validations" do
+    it { should validate_presence_of :minimum }
+    it { should validate_presence_of :maximum }
+  end
+  describe "associations" do
+    it { should belongs_to :category }
+  end
+end
