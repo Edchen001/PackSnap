a81446e2f2fa535cead03a745acdc6ee76d34fa2
edchen001 <edchen001@gmail.com>
2015-01-05 10:01:28 -0800

    add validation to item image content type

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,6 @@ class Item < ActiveRecord::Base
   has_many :categories, through: :suggestions
 
   has_attached_file :image, styles: { :medium => "300x300!", :thumb => "100x100>" }, whiny: false
-  do_not_validate_attachment_file_type :image
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
